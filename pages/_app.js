9bd47990e544ee0134fb083a076ec352caf7e815
Krastan Dimitrov <krastan.kdd@gmail.com>
2022-11-02 00:04:51 -0500

    Add webvitals log in development

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,8 @@ import "../styles/global.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export function reportWebVitals(metric) {
+  process.env.NODE_ENV === "development" &&
+    console.log(metric.name, " ", metric.value);
+}
